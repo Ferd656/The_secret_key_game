e725b451bf9aba55838e1e883ecb9f0714d56b87
Ferdinand <ferd656@gmail.com>
2019-01-06 10:14:51 -0600

    First commit
    Signed-off-by: Ferdinand <ferd656@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 """
     'The secret key game'
     Code by: Ferdinand Feoli
-    Developer's profile: https:ferd656.wixsite.com/profile
+    Developer's profile: www.linkedin.com/in/ferdinandfeoli
 
     Description: A simple game that consists in  guessing
     a 'secret key', you win when you type it.
+
+    The making of this game is based on what is posted here:
+    https://stackoverflow.com/questions/24072790/detect-key-press-in-python
 
     Just for fun.
     You can use it as you want.
